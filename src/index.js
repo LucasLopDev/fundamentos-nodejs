f0831b7328138bf6bfe6c21bf1098d980ee6cad7
Lucas <lopes0022@hotmail.com>
2022-09-30 15:29:27 -0300

    Cadastro

--- src/index.js
+++ src/index.js
@@ -1,37 +1,35 @@
+const { response } = require("express");
 const express = require('express');
+const { v4: uuidv4 } = require("uuid");
 
 const app = express();
 
+const customers = [];
+
 app.use(express.json());
 
+/**
+ * cpf - string
+ * name - string
+ * id - uuid
+ * statement [] 
+*/
 
-app.get("/courses", (request, response) => {
-  const query = request.query;
-  console.log(query);
-  return response.json(["Curso 1", "Curso 2", "Curso 3"]);
-});
+app.post ("/account", (request, response) => {
+  const {cpf, name} = request.body;
 
-app.post("/courses", (request, response) => {
-  const body = request.body;
-  console.log(body);
+  const id = uuidv4();
 
-  return response.json(["Curso 1", "Curso 2", "Curso 3", "Curso 4"]);
-});
+  customers.push({
+      cpf,
+      name,
+      id,
+      statement: [],
+  });
 
-app.put("/courses/:id", (request, response) => {
-  const params = request.params;
-  console.log(params);
-
-  return response.json(["Curso 6", "Curso 2", "Curso 3", "Curso 4"]);
-});
-
-app.patch("/courses/:id", (request, response) => {
-  return response.json(["Curso 6", "Curso 7", "Curso 3", "Curso 4"]);
-});
-
-app.delete("/courses/:id", (request, response) => {
-  return response.json(["Curso 6", "Curso 2", "Curso 4"]);
-});
+  return response.status(201).send();
+  
+})
 
 
 
